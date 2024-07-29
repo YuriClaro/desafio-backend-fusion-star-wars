c319830db904286346bda1fcdf805c19ba5e64ea
Yuri Claro <yuri.claro@outlook.com>
2024-07-29 15:35:37 -0300

    chore: setup TypeORM and database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,35 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PlanetsModule } from './planets/planets.module';
+import { StarSystemsModule } from './star-systems/star-systems.module';
+import { CharactersModule } from './characters/characters.module';
+import { SpaceShipsModule } from './space-ships/space-ships.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Planet } from './planets/planet.entity';
+import { StarSystem } from './star-systems/star-system.entity';
+import { Character } from './characters/character.entity';
+import { SpaceShip } from './space-ships/space-ship.entity';
+
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '1234',
+      database: 'star-wars-db',
+      entities: [Planet, StarSystem, Character, SpaceShip],
+      synchronize: true,
+    }),
+    PlanetsModule,
+    StarSystemsModule,
+    CharactersModule,
+    SpaceShipsModule,
+    StarSystemsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

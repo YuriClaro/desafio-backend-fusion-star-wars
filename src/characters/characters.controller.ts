dc54938c500a3313db1c75ce87eb33de1367ff06
Yuri Claro <yuri.claro@outlook.com>
2024-07-29 15:57:00 -0300

    feat: create Character controller and endpoints

---
+++ src/characters/characters.controller.ts
@@ -0,0 +1,33 @@
+import { Controller, Get, Post, Body, Put, Param, Delete } from '@nestjs/common';
+import { CharactersService } from './characters.service';
+import { Character } from './character.entity';
+
+@Controller('characters')
+export class CharactersController {
+    constructor(private readonly characterService: CharactersService) {}
+
+    @Post()
+    create(@Body() createCharacteraDto: Character): Promise<Character> {
+        return this.characterService.create(createCharacteraDto);
+    }
+
+    @Get()
+    findAll(): Promise<Character[]> {
+        return this.characterService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: number): Promise<Character> {
+        return this.characterService.findCharacterById(id);
+    }
+
+    @Put(':id')
+    update(@Param('id') id: number, @Body() updatedCharacterDto: Character): Promise<Character> {
+        return this.characterService.update(id, updatedCharacterDto);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: number): Promise<void> {
+        return this.characterService.remove(id);
+    }
+}

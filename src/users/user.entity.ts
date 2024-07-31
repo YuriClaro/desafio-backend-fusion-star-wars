a6c6826620f6ff56d010e0d65319b15fa1ae1a7b
Yuri Claro <yuri.claro@outlook.com>
2024-07-31 09:40:10 -0300

    feat: improve robustness of UserModule, UserService and UserEntity

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,6 +1,14 @@
+import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
 export class User {
+  @PrimaryGeneratedColumn()
     userId: number;
+
+    @Column(({ unique: true }))
     username: string;
+
+    @Column()
     password: string;
   }
   
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,22 +1,16 @@
 import { Injectable } from '@nestjs/common';
-
-interface User {
-  userId: number;
-  username: string;
-  password: string;
-}
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
 export class UsersService {
-  private readonly users: User[] = [
-    {
-      userId: 1,
-      username: 'test',
-      password: 'test',
-    },
-  ];
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>
+  ) {}
 
   async findOne(username: string): Promise<User | undefined> {
-    return this.users.find(user => user.username === username);
+    return this.usersRepository.findOne({where: { username }});
   }
 }

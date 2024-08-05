c46318ec936fd96943f67a03e93b9553af7e4926
Yuri Claro <yuri.claro@outlook.com>
2024-08-05 16:20:06 -0300

    feat: add swagger documentation on character

--- src/characters/character.entity.ts
+++ src/characters/character.entity.ts
@@ -1,20 +1,26 @@
 import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
 import { Planet } from '../planets/planet.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity()
 export class Character {
     @PrimaryGeneratedColumn()
+    @ApiProperty({ example: 1, description: 'ID do personagem' })
     id: number;
 
     @Column()
+    @ApiProperty({ example: 'Luke Skywalker', description: 'Nome do personagem' })
     name: string;
 
     @Column()
+    @ApiProperty({ example: 'Human', description: 'Raça do personagem' })
     race: string;
 
     @Column()
+    @ApiProperty({ example: 'Jedi', description: 'Afiliação do personagem' })
     affiliation: string;
 
     @ManyToOne(() => Planet, planet => planet.id)
+    @ApiProperty({ type: () => Planet, description: 'Planeta de origem do personagem' })
     homePlanet: Planet;
 }
--- src/characters/characters.controller.ts
+++ src/characters/characters.controller.ts
@@ -3,14 +3,21 @@ import { CharactersService } from './characters.service';
 import { Character } from './character.entity';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { ValidationException } from 'src/exceptions/validation.exception';
+import { ApiOperation, ApiParam, ApiResponse, ApiTags, ApiBearerAuth } from '@nestjs/swagger';
 
+@ApiTags('Characters')
 @Controller('characters')
+@ApiBearerAuth()
 export class CharactersController {
     constructor(private readonly characterService: CharactersService) {}
 
     @UseGuards(JwtAuthGuard)
     @Post()
     @UsePipes(new ValidationPipe({ transform: true, forbidNonWhitelisted: true}))
+    @ApiOperation({ summary: 'Criar um novo personagem.'})
+    @ApiResponse({ status: 201, description: 'O personagem foi criado com sucesso.'})
+    @ApiResponse({ status: 400, description: 'Solicitação incorreta, cheque as informações.'})
+    @ApiResponse({ status: 500, description: 'Erro inesperado enquanto o personagem estava sendo criado.'})
     async create(@Body() createCharacteraDto: Character): Promise<Character> {
         try {
             return await this.characterService.create(createCharacteraDto);
@@ -23,6 +30,10 @@ export class CharactersController {
         }
     }
 
+    @ApiOperation({ summary: 'Retornando todos os personagens.'})
+    @ApiResponse({ status: 201, description: 'Retornando todos os personagens.'})
+    @ApiResponse({ status: 400, description: 'Nenhum personagem encontrado.'})
+    @ApiResponse({ status: 500, description: 'Erro inesperado enquanto retornavem o personagem criado.'})
     @Get()
     async findAll(): Promise<Character[]> {
         try {
@@ -37,6 +48,12 @@ export class CharactersController {
     }
 
     @Get(':id')
+    @ApiOperation({ summary: 'Retornando um personagem por ID',})
+    @ApiParam({ name: 'id', required: true, description: 'Personagem ID', type: Number })
+    @ApiResponse({ status: 200, description: 'Personagem retornado com sucesso.' })
+    @ApiResponse({ status: 404, description: 'Personagem não encontrado.' })
+    @ApiResponse({ status: 400, description: 'Formato inválido.' })
+    @ApiResponse({ status: 500, description: 'Um erro inesperado aconteceu enquanto recuperava o personagem.' })
     async findOne(@Param('id') id: number): Promise<Character> {
         try {
             const character = await this.characterService.findCharacterById(id);
@@ -55,6 +72,12 @@ export class CharactersController {
     @UseGuards(JwtAuthGuard)
     @Put(':id')
     @UsePipes(new ValidationPipe({ transform: true, forbidNonWhitelisted: true}))
+    @ApiOperation({ summary: 'Atualizando um personagem por ID' })
+    @ApiParam({ name: 'id', required: true, description: 'Personagem ID', type: Number  })
+    @ApiResponse({ status: 200, description: 'Retornando o personagem.' })
+    @ApiResponse({ status: 404, description: 'Personagem não encontrado.' })
+    @ApiResponse({ status: 400, description: 'Formato inválido.' })
+    @ApiResponse({ status: 500, description: 'Um erro inesperado aconteceu enquanto atualizava o personagem.' })
     async update(@Param('id') id: number, @Body() updatedCharacterDto: Character): Promise<Character> {
         try {
             const updatedCharacter = await this.characterService.update(id, updatedCharacterDto);
@@ -76,11 +99,17 @@ export class CharactersController {
 
     @UseGuards(JwtAuthGuard)
     @Delete(':id')
+    @ApiOperation({ summary: 'Deletando um personagem por ID' })
+    @ApiParam({ name: 'id', required: true, description: 'Personagem ID', type: Number  })
+    @ApiResponse({ status: 200, description: 'Retornando o personagem.' })
+    @ApiResponse({ status: 404, description: 'Personagem não encontrado.' })
+    @ApiResponse({ status: 400, description: 'Formato inválido.' })
+    @ApiResponse({ status: 500, description: 'Um erro inesperado aconteceu enquanto deletava o personagem.' })
     async remove(@Param('id') id: number): Promise<void> {
         try {
             const result = await this.characterService.remove(id);
             if (!result) {
-                throw new NotFoundException(`Planeta com o ID: ${id} não encontrado.`);
+                throw new NotFoundException(`Personagem com o ID: ${id} não encontrado.`);
             }
         } catch (error) {
             if (error instanceof ValidationException) {

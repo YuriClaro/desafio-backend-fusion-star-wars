bf3f7b34624d937a31f1d63106fc4b2e873cc5ba
Yuri Claro <yuri.claro@outlook.com>
2024-08-05 16:20:49 -0300

    feat: add swagger documentation on star-system

--- src/star-systems/star-system.entity.ts
+++ src/star-systems/star-system.entity.ts
@@ -1,17 +1,22 @@
 import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
 import { Planet } from '../planets/planet.entity'
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity()
 export class StarSystem {
     @PrimaryGeneratedColumn()
+    @ApiProperty({ example: 1, description: 'O identificador único do sistema estelar' })
     id: number;
 
     @Column()
+    @ApiProperty({ example: 'Sistema Solar', description: 'O nome do sistema estelar' })
     name: string;
 
     @Column()
+    @ApiProperty({ example: 'Um sistema estelar que contém o planeta Terra.', description: 'A descrição do sistema estelar' })
     description: string;
 
     @OneToMany(() => Planet, planet => planet.id)
+    @ApiProperty({ type: () => [Planet], description: 'Lista de planetas no sistema estelar' })
     planets:  Planet[];
 }
--- src/star-systems/star-systems.controller.ts
+++ src/star-systems/star-systems.controller.ts
@@ -3,7 +3,9 @@ import { StarSystemsService } from './star-systems.service';
 import { StarSystem } from './star-system.entity';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { ValidationException } from 'src/exceptions/validation.exception';
+import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Star Systems')
 @Controller('star-systems')
 export class StarSystemsController {
     constructor(private readonly starSystemService: StarSystemsService) {}
@@ -11,6 +13,10 @@ export class StarSystemsController {
     @UseGuards(JwtAuthGuard)
     @Post()
     @UsePipes(new ValidationPipe({ transform: true, forbidNonWhitelisted: true}))
+    @ApiOperation({ summary: 'Cria um novo sistema estelar' })
+    @ApiResponse({ status: 201, description: 'O sistema estelar foi criado com sucesso.' })
+    @ApiResponse({ status: 400, description: 'Solicitação inválida, verifique as informações.' })
+    @ApiResponse({ status: 500, description: 'Um erro inesperado aconteceu enquanto o sistema estelar estava sendo criado.' })
     async create(@Body() createStarSystemaDto: StarSystem): Promise<StarSystem> {
         try {
             return await this.starSystemService.create(createStarSystemaDto);
@@ -24,6 +30,10 @@ export class StarSystemsController {
     }
 
     @Get()
+    @ApiOperation({ summary: 'Recupera todos os sistemas estelares' })
+    @ApiResponse({ status: 200, description: 'Lista de sistemas estelares recuperada com sucesso.' })
+    @ApiResponse({ status: 404, description: 'Nenhum sistema estelar foi encontrado.' })
+    @ApiResponse({ status: 500, description: 'Um erro inesperado aconteceu enquanto recuperava os sistemas estelares.' })
     async findAll(): Promise<StarSystem[]> {
         try {
             const starSystem = await this.starSystemService.findAll();
@@ -37,6 +47,11 @@ export class StarSystemsController {
     }
 
     @Get(':id')
+    @ApiOperation({ summary: 'Recupera um sistema estelar pelo ID' })
+    @ApiParam({ name: 'id', description: 'O identificador único do sistema estelar', type: Number })
+    @ApiResponse({ status: 200, description: 'Sistema estelar recuperado com sucesso.' })
+    @ApiResponse({ status: 404, description: 'Sistema estelar com o ID fornecido não foi encontrado.' })
+    @ApiResponse({ status: 500, description: 'Um erro inesperado aconteceu enquanto recuperava o sistema estelar.' })
     async findOne(@Param('id') id: number): Promise<StarSystem> {
         try {
             const starSystem = await this.starSystemService.findStarSystemById(id);
@@ -56,6 +71,12 @@ export class StarSystemsController {
     @UseGuards(JwtAuthGuard)
     @Put(':id')
     @UsePipes(new ValidationPipe({ transform: true, forbidNonWhitelisted: true}))
+    @ApiOperation({ summary: 'Atualiza um sistema estelar pelo ID' })
+    @ApiParam({ name: 'id', description: 'O identificador único do sistema estelar', type: Number })
+    @ApiResponse({ status: 200, description: 'Sistema estelar atualizado com sucesso.' })
+    @ApiResponse({ status: 400, description: 'Solicitação inválida, verifique as informações.' })
+    @ApiResponse({ status: 404, description: 'Sistema estelar com o ID fornecido não foi encontrado.' })
+    @ApiResponse({ status: 500, description: 'Um erro inesperado aconteceu enquanto atualizava o sistema estelar.' })
     async update(@Param('id') id: number, @Body() updatedStarSystemDto: StarSystem): Promise<StarSystem> {
         try {
             const updatedStarSystem = await this.starSystemService.update(id, updatedStarSystemDto);
@@ -76,6 +97,11 @@ export class StarSystemsController {
 
     @UseGuards(JwtAuthGuard)
     @Delete(':id')
+    @ApiOperation({ summary: 'Remove um sistema estelar pelo ID' })
+    @ApiParam({ name: 'id', description: 'O identificador único do sistema estelar', type: Number })
+    @ApiResponse({ status: 200, description: 'Sistema estelar removido com sucesso.' })
+    @ApiResponse({ status: 404, description: 'Sistema estelar com o ID fornecido não foi encontrado.' })
+    @ApiResponse({ status: 500, description: 'Um erro inesperado aconteceu enquanto removia o sistema estelar.' })
     async remove(@Param('id') id: number): Promise<void> {
         try {
             const result = await this.starSystemService.remove(id);
